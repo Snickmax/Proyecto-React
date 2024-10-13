2b25c0c676ce53bfe8c0684ca3a28c47d412ddc5
Snickmax <victor.2002.espinoza@gmail.com>
2024-10-13 18:01:56 -0300

    traduccion de las plantas

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -7,206 +7,206 @@ function ProductList() {
 
     const plantsArray = [
         {
-            category: "Air Purifying Plants",
+            category: "Plantas purificadoras de aire",
             plants: [
                 {
-                    name: "Snake Plant",
+                    name: "Planta serpiente",
                     image: "https://cdn.pixabay.com/photo/2021/01/22/06/04/snake-plant-5939187_1280.jpg",
-                    description: "Produces oxygen at night, improving air quality.",
+                    description: "Produce oxígeno por la noche, mejorando la calidad del aire.",
                     cost: "$15"
                 },
                 {
-                    name: "Spider Plant",
+                    name: "Planta araña",
                     image: "https://cdn.pixabay.com/photo/2018/07/11/06/47/chlorophytum-3530413_1280.jpg",
-                    description: "Filters formaldehyde and xylene from the air.",
+                    description: "Filtra el formaldehído y el xileno del aire.",
                     cost: "$12"
                 },
                 {
-                    name: "Peace Lily",
+                    name: "Lirio de la paz",
                     image: "https://cdn.pixabay.com/photo/2019/06/12/14/14/peace-lilies-4269365_1280.jpg",
-                    description: "Removes mold spores and purifies the air.",
+                    description: "Elimina las esporas de moho y purifica el aire.",
                     cost: "$18"
                 },
                 {
-                    name: "Boston Fern",
+                    name: "Helecho de Boston",
                     image: "https://cdn.pixabay.com/photo/2020/04/30/19/52/boston-fern-5114414_1280.jpg",
-                    description: "Adds humidity to the air and removes toxins.",
+                    description: "Agrega humedad al aire y elimina toxinas.",
                     cost: "$20"
                 },
                 {
-                    name: "Rubber Plant",
+                    name: "Planta de caucho",
                     image: "https://cdn.pixabay.com/photo/2020/02/15/11/49/flower-4850729_1280.jpg",
-                    description: "Easy to care for and effective at removing toxins.",
+                    description: "Fácil de cuidar para y eficaz para eliminar toxinas.",
                     cost: "$17"
                 },
                 {
                     name: "Aloe Vera",
                     image: "https://cdn.pixabay.com/photo/2018/04/02/07/42/leaf-3283175_1280.jpg",
-                    description: "Purifies the air and has healing properties for skin.",
+                    description: "Purifica el aire y tiene propiedades curativas para la piel.",
                     cost: "$14"
                 }
             ]
         },
         {
-            category: "Aromatic Fragrant Plants",
+            category: "Plantas Aromáticas Fragantes",
             plants: [
                 {
-                    name: "Lavender",
+                    name: "Lavanda",
                     image: "https://images.unsplash.com/photo-1611909023032-2d6b3134ecba?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                    description: "Calming scent, used in aromatherapy.",
+                    description: "Aroma relajante, utilizado en aromaterapia.",
                     cost: "$20"
                 },
                 {
-                    name: "Jasmine",
+                    name: "Jazmín",
                     image: "https://images.unsplash.com/photo-1592729645009-b96d1e63d14b?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                    description: "Sweet fragrance, promotes relaxation.",
+                    description: "Fragancia dulce, promueve la relajación.",
                     cost: "$18"
                 },
                 {
-                    name: "Rosemary",
+                    name: "Romero",
                     image: "https://cdn.pixabay.com/photo/2019/10/11/07/12/rosemary-4541241_1280.jpg",
-                    description: "Invigorating scent, often used in cooking.",
+                    description: "Aroma vigorizante, a menudo utilizado en la cocina.",
                     cost: "$15"
                 },
                 {
-                    name: "Mint",
+                    name: "Melisa",
                     image: "https://cdn.pixabay.com/photo/2016/01/07/18/16/mint-1126282_1280.jpg",
-                    description: "Refreshing aroma, used in teas and cooking.",
+                    description: "Aroma refrescante, utilizado en tés y cocina.",
                     cost: "$12"
                 },
                 {
-                    name: "Lemon Balm",
+                    name: "Melisa",
                     image: "https://cdn.pixabay.com/photo/2019/09/16/07/41/balm-4480134_1280.jpg",
                     description: "Citrusy scent, relieves stress and promotes sleep.",
                     cost: "$14"
                 },
                 {
-                    name: "Hyacinth",
+                    name: "Jacinto",
                     image: "https://cdn.pixabay.com/photo/2019/04/07/20/20/hyacinth-4110726_1280.jpg",
-                    description: "Hyacinth is a beautiful flowering plant known for its fragrant.",
+                    description: "Aroma cítrico, alivia el estrés y favorece el sueño.",
                     cost: "$22"
                 }
             ]
         },
         {
-            category: "Insect Repellent Plants",
+            category: "Plantas repelentes de insectos",
             plants: [
                 {
-                    name: "oregano",
+                    name: "Orégano",
                     image: "https://cdn.pixabay.com/photo/2015/05/30/21/20/oregano-790702_1280.jpg",
-                    description: "The oregano plants contains compounds that can deter certain insects.",
+                    description: "Las plantas de orégano contienen compuestos que pueden disuadir a ciertos insectos.",
                     cost: "$10"
                 },
                 {
-                    name: "Marigold",
+                    name: "Caléndula",
                     image:"https://cdn.pixabay.com/photo/2022/02/22/05/45/marigold-7028063_1280.jpg",
-                    description: "Natural insect repellent, also adds color to the garden.",
+                    description: "Repelente natural de insectos, también agrega color al jardín.",
                     cost: "$8"
                 },
                 {
-                    name: "Geraniums",
+                    name: "Geranios",
                     image: "https://cdn.pixabay.com/photo/2012/04/26/21/51/flowerpot-43270_1280.jpg",
-                    description: "Known for their insect-repelling properties while adding a pleasant scent.",
+                    description: "Conocidas por sus propiedades repelentes de insectos y por su agradable aroma.",
                     cost: "$20"
                 },
                 {
-                    name: "Basil",
+                    name: "Albahaca",
                     image: "https://cdn.pixabay.com/photo/2016/07/24/20/48/tulsi-1539181_1280.jpg",
-                    description: "Repels flies and mosquitoes, also used in cooking.",
+                    description: "Repele moscas y mosquitos, también se usa en la cocina.",
                     cost: "$9"
                 },
                 {
-                    name: "Lavender",
+                    name: "Lavanda",
                     image: "https://images.unsplash.com/photo-1611909023032-2d6b3134ecba?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                    description: "Calming scent, used in aromatherapy.",
+                    description: "Aroma calmante, usado en aromaterapia.",
                     cost: "$20"
                 },
                 {
                     name: "Catnip",
                     image: "https://cdn.pixabay.com/photo/2015/07/02/21/55/cat-829681_1280.jpg",
-                    description: "Repels mosquitoes and attracts cats.",
+                    description: "Repele mosquitos y atrae gatos.",
                     cost: "$13"
                 }
             ]
         },
         {
-            category: "Medicinal Plants",
+            category: "Plantas medicinales",
             plants: [
                 {
                     name: "Aloe Vera",
                     image: "https://cdn.pixabay.com/photo/2018/04/02/07/42/leaf-3283175_1280.jpg",
-                    description: "Soothing gel used for skin ailments.",
+                    description: "Gel calmante utilizado para afecciones de la piel.",
                     cost: "$14"
                 },
                 {
-                    name: "Echinacea",
+                    name: "Equinácea",
                     image: "https://cdn.pixabay.com/photo/2014/12/05/03/53/echinacea-557477_1280.jpg",
-                    description: "Boosts immune system, helps fight colds.",
+                    description: "Fortalece el sistema inmunológico, ayuda a combatir los resfriados.",
                     cost: "$16"
                 },
                 {
                     name: "Peppermint",
                     image: "https://cdn.pixabay.com/photo/2017/07/12/12/23/peppermint-2496773_1280.jpg",
-                    description: "Relieves digestive issues and headaches.",
+                    description: "Alivia los problemas digestivos y los dolores de cabeza.",
                     cost: "$13"
                 },
                 {
-                    name: "Lemon Balm",
+                    name: "Melisa",
                     image: "https://cdn.pixabay.com/photo/2019/09/16/07/41/balm-4480134_1280.jpg",
-                    description: "Calms nerves and promotes relaxation.",
+                    description: "Calma los nervios y promueve la relajación.",
                     cost: "$14"
                 },
                 {
-                    name: "Chamomile",
+                    name: "Manzanilla",
                     image: "https://cdn.pixabay.com/photo/2016/08/19/19/48/flowers-1606041_1280.jpg",
-                    description: "Soothes anxiety and promotes sleep.",
+                    description: "Alivia la ansiedad y favorece el sueño.",
                     cost: "$15"
                 },
                 {
-                    name: "Calendula",
+                    name: "Caléndula",
                     image: "https://cdn.pixabay.com/photo/2019/07/15/18/28/flowers-4340127_1280.jpg",
-                    description: "Heals wounds and soothes skin irritations.",
+                    description: "Cura heridas y alivia irritaciones de la piel.",
                     cost: "$12"
                 }
             ]
         },
         {
-            category: "Low Maintenance Plants",
+            category: "Plantas de bajo mantenimiento",
             plants: [
                 {
                     name: "ZZ Plant",
                     image: "https://images.unsplash.com/photo-1632207691143-643e2a9a9361?q=80&w=464&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                    description: "Thrives in low light and requires minimal watering.",
+                    description: "Prospera con poca luz y requiere un riego mínimo.",
                     cost: "$25"
                 },
                 {
                     name: "Pothos",
                     image: "https://cdn.pixabay.com/photo/2018/11/15/10/32/plants-3816945_1280.jpg",
-                    description: "Tolerates neglect and can grow in various conditions.",
+                    description: "Tolera el descuido y puede crecer en diversas condiciones.",
                     cost: "$10"
                 },
                 {
-                    name: "Snake Plant",
+                    name: "Planta serpiente",
                     image: "https://cdn.pixabay.com/photo/2021/01/22/06/04/snake-plant-5939187_1280.jpg",
-                    description: "Needs infrequent watering and is resilient to most pests.",
+                    description: "Necesita riego poco frecuente y es resistente a la mayoría de las plagas.",
                     cost: "$15"
                 },
                 {
-                    name: "Cast Iron Plant",
+                    name: "Planta de hierro fundido",
                     image: "https://cdn.pixabay.com/photo/2017/02/16/18/04/cast-iron-plant-2072008_1280.jpg",
-                    description: "Hardy plant that tolerates low light and neglect.",
+                    description: "Planta resistente que tolera poca luz y descuido.",
                     cost: "$20"
                 },
                 {
-                    name: "Succulents",
+                    name: "Suculentas",
                     image: "https://cdn.pixabay.com/photo/2016/11/21/16/05/cacti-1846147_1280.jpg",
-                    description: "Drought-tolerant plants with unique shapes and colors.",
+                    description: "Plantas resistentes a la sequía con formas y colores únicos.",
                     cost: "$18"
                 },
                 {
                     name: "Aglaonema",
                     image: "https://cdn.pixabay.com/photo/2014/10/10/04/27/aglaonema-482915_1280.jpg",
-                    description: "Requires minimal care and adds color to indoor spaces.",
+                    description: "Requiere un cuidado mínimo y agrega color a los espacios interiores.",
                     cost: "$22"
                 }
             ]
